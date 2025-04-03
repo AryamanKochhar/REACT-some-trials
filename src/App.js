8a3d260fdcdfb4afffe6f6c14d12cee9f993784c
Aryaman Kochhar <aryamankochhar456@gmail.com>
2025-04-03 15:31:34 +0530

    cac

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import Calculator from './Calculator';
 
 function counterapp1() {
-  const [count,setc]=useState(0);
   
   return (
     <div style={{textAlign:"center"}}>
