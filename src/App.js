f89d194017008779a97b624c0670018b264931a8
Aryaman Kochhar <aryamankochhar456@gmail.com>
2025-04-10 03:13:27 +0530

    function_welcome

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import Calculator from './Counter';
+import Welcome from './function_welcome';
 
 function Counterapp1() {
   
   return (
     <div style={{textAlign:"center"}}>
-      <Calculator />
+      {/* <Calculator /> */}
+      <Welcome name="Aryaman" />;
     </div>
   )
 }
---
+++ src/function_welcome.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Welcome(props){
+  return <h2>hello 
+    {props.name}</h2>;
+}
+export default Welcome;
